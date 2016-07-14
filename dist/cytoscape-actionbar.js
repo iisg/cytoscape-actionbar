e941acfae5d3743eb5f272694b7dfa73c1c504e1
Paulina <paulina.b.karp@gmail.com>
2016-07-14 09:02:04 +0200

    Add class per item group

--- dist/cytoscape-actionbar.js
+++ dist/cytoscape-actionbar.js
@@ -1,23 +1,20 @@
 angular.module('cytoscape.actionbar', []).directive('cytoscapeActionbar', ["$timeout", function($timeout) {
   return {
-    template: "<div class=\"cytoscape-graph-actionbar\" style=\"right: 20px; position: absolute; z-index: 1000\">\n  <div ng-class=\"options.actionbarClass\" ng-repeat=\"group in options.items\">\n      <button ng-repeat=\"item in group\" ng-click='item.action(cy, item)'\n        ng-disabled='!item.enabled()' ng-if='item.visible()'\n        ng-class='[options.actionItemClass, icon, item.icon]' data-container='body' bs-tooltip='item.tooltip'>\n      </button>\n  </div>\n</div>",
+    template: "<div class=\"cytoscape-graph-actionbar\" style=\"right: 0; left: 0; position: absolute; z-index: 1000\">\n  <div ng-repeat=\"group in options.itemGroups\" ng-class=\"group.itemGroupClass\">\n      <button ng-repeat=\"item in group.items\" ng-click='item.action(cy, item)'\n        ng-disabled='!item.enabled()' ng-if='item.visible()'\n        ng-class='[options.actionItemClass, icon, item.icon]' data-container='body' bs-tooltip='item.tooltip'>\n      </button>\n  </div>\n</div>",
     link: function(scope) {
       return cytoscape('core', 'actionbar', function(options) {
-        var defaults, digestEvent, digestTimeout, group, i, item, j, k, len, len1, len2, ref, ref1, runDigestAfterEvent;
+        var defaults, digestEvent, digestTimeout, group, i, item, j, k, len, len1, len2, ref, ref1, ref2, runDigestAfterEvent;
         defaults = {
-          items: [],
-          actionbarClass: 'ui-cytoscape-actionbar',
+          itemGroups: [],
           actionItemClass: 'action-item'
         };
         angular.extend(defaults, options);
-        if (options.items[0].action) {
-          options.items = [options.items];
-        }
-        ref = options.items;
+        ref = options.itemGroups;
         for (i = 0, len = ref.length; i < len; i++) {
           group = ref[i];
-          for (j = 0, len1 = group.length; j < len1; j++) {
-            item = group[j];
+          ref1 = group.items;
+          for (j = 0, len1 = ref1.length; j < len1; j++) {
+            item = ref1[j];
             if (item.visible == null) {
               item.visible = function() {
                 return true;
@@ -39,9 +36,9 @@ angular.module('cytoscape.actionbar', []).directive('cytoscapeActionbar', ["$tim
             return digestTimeout = null;
           }), 100);
         };
-        ref1 = ['zoom', 'pan'];
-        for (k = 0, len2 = ref1.length; k < len2; k++) {
-          digestEvent = ref1[k];
+        ref2 = ['zoom', 'pan'];
+        for (k = 0, len2 = ref2.length; k < len2; k++) {
+          digestEvent = ref2[k];
           this.on(digestEvent, runDigestAfterEvent);
         }
         return scope.options = options;
--- src/CytoscapeActionbar.coffee
+++ src/CytoscapeActionbar.coffee
@@ -1,8 +1,8 @@
 angular.module('cytoscape.actionbar', []).directive 'cytoscapeActionbar', ($timeout) ->
   template: """
-    <div class="cytoscape-graph-actionbar" style="right: 20px; position: absolute; z-index: 1000">
-      <div ng-class="options.actionbarClass" ng-repeat="group in options.items">
-          <button ng-repeat="item in group" ng-click='item.action(cy, item)'
+    <div class="cytoscape-graph-actionbar" style="right: 0; left: 0; position: absolute; z-index: 1000">
+      <div ng-repeat="group in options.itemGroups" ng-class="group.itemGroupClass">
+          <button ng-repeat="item in group.items" ng-click='item.action(cy, item)'
             ng-disabled='!item.enabled()' ng-if='item.visible()'
             ng-class='[options.actionItemClass, icon, item.icon]' data-container='body' bs-tooltip='item.tooltip'>
           </button>
@@ -15,16 +15,12 @@ angular.module('cytoscape.actionbar', []).directive 'cytoscapeActionbar', ($time
       'actionbar',
       (options) ->
         defaults =
-          items: []
-          actionbarClass: 'ui-cytoscape-actionbar' # set a class name for the toolbar to help with styling
+          itemGroups: []
           actionItemClass: 'action-item' # set a class name for a toolbar item to help with styling
         angular.extend(defaults, options)
 
-        if options.items[0].action
-          options.items = [options.items]
-
-        for group in options.items
-          for item in group
+        for group in options.itemGroups
+          for item in group.items
             item.visible ?= -> true
             item.enabled ?= -> true
 
